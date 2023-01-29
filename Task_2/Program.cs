15c6349165685183b105f2ec59d7ec27e40b4270
Сергей Челпанов <S.inter.4@mail.ru>
2023-01-29 14:11:18 +0300

    задача_1

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int num =int.Parse(Console.ReadLine()!);
+
+if (num % 100 != 0 )
+{
+    Console.WriteLine($"Трехзначное число");
+}
+else
+{
+    Console.WriteLine("Это НЕ ТРЁХЗНАЧНОЕ число");
+}
